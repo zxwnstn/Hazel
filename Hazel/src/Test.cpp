7e0883447fb2218df0c11d95053983faf0f384cb
zxwnstn <zxwnstn@naver.com>
2019-12-29 23:13:07 +0900

    first hazel

---
+++ Hazel/src/Test.cpp
@@ -0,0 +1,8 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace Hazel {
+	void print() {
+		printf("Welcome to Hazel Engine\n");
+	}
+}
---
+++ SandBox/src/Application.cpp
@@ -0,0 +1,7 @@
+namespace Hazel {
+	__declspec(dllimport) void print();
+}
+
+void main() {
+	Hazel::print();
+}
