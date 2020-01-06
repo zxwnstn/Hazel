b7a913539d2c5e8a8a5fd51614c60c825d177831
zxwnstn <zxwnstn@naver.com>
2020-01-07 03:26:10 +0900

    window event

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,22 +1,34 @@
 #include "hzpch.h"
 #include "Application.h"
 
-#include "Events/ApplicationEvent.h"
+
 #include "Hazel/Log.h"
 
 #include "GLFW/glfw3.h"
 
 namespace Hazel {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 
 	Application::~Application()
 	{
 	}
+
+	void Application::OnEvent(Event & e)
+	{
+		EventDispatcher dispatcher(e);
+
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnwindowClose));
+		HZ_CORE_TRACE("{0}", e);
+	}
+
+
 
 	void Application::Run()
 	{
@@ -26,6 +38,12 @@ namespace Hazel {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnwindowClose(WindowCloseEvent & e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
