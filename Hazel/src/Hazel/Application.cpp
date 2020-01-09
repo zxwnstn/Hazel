ad2d7c2872474baa5fa113901e76cabbf00e59e0
zxwnstn <zxwnstn@naver.com>
2020-01-09 20:51:07 +0900

    layer

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -26,9 +26,24 @@ namespace Hazel {
 
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnwindowClose));
 		HZ_CORE_TRACE("{0}", e);
+
+		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();) {
+			(*--it)->OnEvent(e);
+			if (e.m_Handled)
+				break;
+		}
+
 	}
 
+	void Application::PushLayer(Layer * layer)
+	{
+		m_LayerStack.PushLayer(layer);
+	}
 
+	void Application::PushOverlay(Layer * layer)
+	{
+		m_LayerStack.PushOverlay(layer);
+	}
 
 	void Application::Run()
 	{
@@ -36,6 +51,10 @@ namespace Hazel {
 		{
 			glClearColor(1, 0, 1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			for (Layer* layer : m_LayerStack)
+				layer->OnUpdate();
+
 			m_Window->OnUpdate();
 		}
 	}
