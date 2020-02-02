f39e3c67abd79b8ae4d2bd43abfd9b4d3555fcc3
zxwnstn <zxwnstn@naver.com>
2020-02-03 02:21:30 +0900

    Key and Mouse Codes

--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -1,6 +1,7 @@
 #include "Hazel.h"
 
-class ExampleLayer : public Hazel::Layer {
+class ExampleLayer 
+	: public Hazel::Layer {
 public:
 	ExampleLayer()
 		: Layer("Example")
@@ -8,11 +9,19 @@ public:
 	}
 
 	void OnUpdate() override {
-		HZ_INFO("ExampleLayer::Update");
+		if (Hazel::Input::IsKeyPressd(HZ_KEY_TAB)) {
+			HZ_TRACE("Tab key is pressed! (poll)");
+		}
 	}
 
 	void OnEvent(Hazel::Event& event) override {
-		HZ_TRACE("{0}", event);
+		if (event.GetEventType() == Hazel::EventType::KeyPressed)
+		{
+			Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
+			if(e.GetKeyCode() == HZ_KEY_TAB)
+				HZ_TRACE("Tab key is pressed! (event)");
+			HZ_TRACE("{0}", (char)e.GetKeyCode());
+		}
 	}
 
 };
