ce007e345ab9dba576bcd6ae5c7a0e94bfdb5ca9
zxwnstn <zxwnstn@naver.com>
2020-02-21 01:14:26 +0900

    Shader

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -8,6 +8,7 @@
 #include "Events/ApplicationEvent.h"
 
 #include "Hazel/ImGui/ImGuiLayer.h"
+#include "Renderer/Shader.h"
 
 namespace Hazel {
 
@@ -35,6 +36,7 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
