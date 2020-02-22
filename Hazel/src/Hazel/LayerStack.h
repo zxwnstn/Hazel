02ca00a85752498e797479d0585391aea9ad0390
zxwnstn <zxwnstn@naver.com>
2020-02-23 03:30:57 +0900

    Vertex Buffer Layout

--- Hazel/src/Hazel/LayerStack.h
+++ Hazel/src/Hazel/LayerStack.h
@@ -15,12 +15,8 @@ namespace Hazel{
 		void PopLayer(Layer* layer);
 		void PopOverlay(Layer* overlay);
 
-		std::vector<Layer*>::iterator begin() {
-			return m_Layers.begin();
-		}
-		std::vector<Layer*>::iterator end() {
-			return m_Layers.end();
-		}
+		std::vector<Layer*>::iterator begin() { return m_Layers.begin(); }
+		std::vector<Layer*>::iterator end() { return m_Layers.end(); }
 	public:
 		std::vector<Layer*> m_Layers;
 		unsigned int m_LayerInsertIndex = 0;
