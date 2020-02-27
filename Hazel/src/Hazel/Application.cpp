e81017a14243ea1ac8698bcd97e3ab516fac330f
zxwnstn <zxwnstn@naver.com>
2020-02-27 16:36:01 +0900

    Move to SandBox

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -3,10 +3,6 @@
 
 #include "Hazel/Log.h"
 
-#include "Hazel/Renderer/Renderer.h"
-
-#include "Input.h"
-
 namespace Hazel {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -14,7 +10,6 @@ namespace Hazel {
 	Application* Application::s_Instance = nullptr;
 
 	Application::Application()
-		: m_Camera(-1.6f, 1.6f, -0.9f, 0.9f)
 	{
 		HZ_CORE_ASSERT(!s_Instance, "Application arleady exist!");
 		s_Instance = this;
@@ -23,120 +18,6 @@ namespace Hazel {
 
 		m_ImGuilayer = new ImGuiLayer;
 		PushOverlay(m_ImGuilayer);
-
-		//triangle
-		//1. Create VertexArray
-		m_VertexArray.reset(VertexArray::Create());
-		//2. define vertices, Indices
-		float vertices[3 * 7] = {
-			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
-			0.5f,  -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
-			0.0f,   0.5f, 0.0f, 0.8f, 0.7f, 0.2f, 1.0f
-		};
-		unsigned int indices[3] = {
-			0, 1, 2
-		};
-		//3. Create VertexBuffer
-		std::shared_ptr<VertexBuffer> vertexBuffer;
-		vertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
-		vertexBuffer->SetLayout({
-			{ShaderDataType::Float3, "a_Position"},
-			{ShaderDataType::Float4, "a_Color"},
-			});
-		m_VertexArray->AddVertexBuffer(vertexBuffer);
-		//4. Create IndexBuffer
-		std::shared_ptr<IndexBuffer> indexBuffer;
-		indexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
-		m_VertexArray->SetIndexBuffer(indexBuffer);
-		//5. define shader
-		std::string vertexSrc = R"(
-			#version 330 core
-
-			layout(location = 0) in vec3 a_Position;			
-			layout(location = 1) in vec4 a_Color;			
-			
-			uniform mat4 u_ViewProjection;
-
-			out vec3 v_Position;			
-			out vec4 v_Color;			
-
-			void main()
-			{
-				v_Position = a_Position;
-				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
-			}
-		)";
-		std::string fragmentSrc = R"(
-			#version 330 core
-
-			layout(location = 0) out vec4 color;			
-			
-			in vec3 v_Position;			
-			in vec4 v_Color;	
-
-			void main()
-			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
-				color = v_Color;
-			}
-		)";
-		m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
-
-		//Square
-		//1. Create VertexArray
-		m_SqaureVA.reset(VertexArray::Create());
-		//2. define vertices, Indices
-		float squareVertices[3 * 4] = {
-			-0.75f,	 -0.75f, 0.0f,
-			 0.75f,  -0.75f, 0.0f,
-			 0.75f,   0.75f, 0.0f,
-			-0.75f,   0.75f, 0.0f
-		};
-		unsigned int squreIndices[6] = {
-			0, 1, 2, 2, 3, 0
-		};
-		//3. Create VertexBuffer
-		std::shared_ptr<VertexBuffer> squareVB;
-		squareVB.reset(VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
-		squareVB->SetLayout({
-			{ShaderDataType::Float3, "a_Position"},
-			});
-		m_SqaureVA->AddVertexBuffer(squareVB);
-		//4. Create IndexBuffer
-		std::shared_ptr<IndexBuffer> squreIB;
-		squreIB.reset(IndexBuffer::Create(squreIndices, sizeof(squreIndices) / sizeof(uint32_t)));
-		m_SqaureVA->SetIndexBuffer(squreIB);
-		//5. define shader
-		std::string blueShadervertexSrc = R"(
-			#version 330 core
-
-			layout(location = 0) in vec3 a_Position;			
-			
-			uniform mat4 u_ViewProjection;
-
-			out vec3 v_Position;			
-
-			void main()
-			{
-				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
-			}
-		)";
-		std::string blueShaderFragmentSrc = R"(
-			#version 330 core
-
-			layout(location = 0) out vec4 color;			
-			
-			in vec3 v_Position;			
-
-			void main()
-			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
-			}
-		)";
-		m_BlueShader.reset(new Shader(blueShadervertexSrc, blueShaderFragmentSrc));
-
 	}
 
 	Application::~Application()
@@ -148,7 +29,6 @@ namespace Hazel {
 		EventDispatcher dispatcher(e);
 
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnwindowClose));
-		//HZ_CORE_TRACE("{0}", e);
 
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();) {
 			(*--it)->OnEvent(e);
@@ -173,19 +53,7 @@ namespace Hazel {
 	{
 		while (m_Running)
 		{
-			RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
-			RenderCommand::Clear();
-
-			m_Camera.SetPosition({ 0.5f, 0.5f, 0.f });
-			m_Camera.SetRotation(45.f);
-
-			Renderer::BeginScene(m_Camera);
-
-			Renderer::Submit(m_BlueShader, m_SqaureVA);
-			Renderer::Submit(m_Shader, m_VertexArray);
-
-			Renderer::EndScene();
-
+			
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
 
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -9,14 +9,9 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
-#include "Renderer/Shader.h"
-#include "Renderer/Buffer.h"
-#include "Renderer/VertexArray.h"
-#include "Renderer/OthographicCamera.h"
-
 namespace Hazel {
 
-	class HAZEL_API Application
+	class Application
 	{
 	public:
 		Application();
@@ -38,14 +33,6 @@ namespace Hazel {
 		ImGuiLayer* m_ImGuilayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
-
-		std::shared_ptr<Shader> m_Shader;
-		std::shared_ptr<VertexArray> m_VertexArray;
-
-		std::shared_ptr<Shader> m_BlueShader;
-		std::shared_ptr<VertexArray> m_SqaureVA;
-
-		OthographicCamera m_Camera;
 	private:
 		static Application* s_Instance;
 	};
