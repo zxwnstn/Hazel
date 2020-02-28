3fe8c80defe1012c80e9b425ae32271e26273936
zxwnstn <zxwnstn@naver.com>
2020-02-28 17:33:02 +0900

    Shader abstaction

--- SandBox/src/SandboxApp.cpp
+++ SandBox/src/SandboxApp.cpp
@@ -1,8 +1,11 @@
 #include "Hazel.h"
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer
 	: public Hazel::Layer {
@@ -68,7 +71,7 @@ public:
 				color = v_Color;
 			}
 		)";
-		m_Shader.reset(new Hazel::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Hazel::Shader::Create(vertexSrc, fragmentSrc));
 
 		//Square
 		//1. Create VertexArray
@@ -118,14 +121,14 @@ public:
 			
 			in vec3 v_Position;			
 			
-			uniform vec4 u_Color;
+			uniform vec3 u_Color;
 
 			void main()
 			{
-				color = u_Color;
+				color = vec4(u_Color, 1.0);
 			}
 		)";
-		m_flatColorShader.reset(new Hazel::Shader(flatColorShadervertexSrc, flatColorShaderFragmentSrc));
+		m_flatColorShader.reset(Hazel::Shader::Create(flatColorShadervertexSrc, flatColorShaderFragmentSrc));
 
 	}
 
@@ -154,15 +157,8 @@ public:
 		
 		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
-		glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0);
-		glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0);
-
-		/*Hazel::MaterialRef material = new Hazel::Materal(m_flatColorShader);
-		Hazel::MaterialInstanceRef mi = new Hazel::MaterialInstance(material);
-
-		mi->SetValue("u_Color", redColor);
-		mi->SetTexture("u_AlbedoMap", texture);
-		squareMesh->SetMaterial(mi);*/
+		std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_flatColorShader)->Bind();
+		std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_flatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
 
 		Hazel::Renderer::BeginScene(m_Camera);
 		for (int y = 0; y < 20; ++y)
@@ -171,10 +167,6 @@ public:
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				if (x % 2 == 0)
-					m_flatColorShader->UploadUniformFloat4("u_Color", redColor);
-				else
-					m_flatColorShader->UploadUniformFloat4("u_Color", blueColor);
 				Hazel::Renderer::Submit(m_flatColorShader, m_SqaureVA, transform);
 			}
 		}
@@ -185,6 +177,9 @@ public:
 
 	void OnImGuiRender() override
 	{
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Sqaure Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(Hazel::Event& event) override
@@ -204,6 +199,8 @@ private:
 
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 30.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public Hazel::Application
